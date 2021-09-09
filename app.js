b46f17c169e48d61d27a2c8a066f3e30301240e7
Md. Abdullah Al Mamun <mamun.cseian@gmail.com>
2021-09-09 06:27:35 +0600

    Express learning

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
+});
+
+app.listen(3000);
