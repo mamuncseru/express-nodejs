a0351d986dd0c930a801ff4a89555ee778c5b32c
Md. Abdullah Al Mamun <mamun.cseian@gmail.com>
2021-09-09 06:45:39 +0600

    path learned

--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,12 @@
 const express = require('express');
-
+const path = require('path');
+const rootDir = require('../util/path');
 const router = express.Router();
 
 router.get('/',(req, res, next)=> {
-    console.log('In another middleware');
-    res.send('<h1>Hello from Express</h1>');
+    // console.log('In another middleware');
+    // res.send('<h1>Hello from Express</h1>');
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
   });
 
 
