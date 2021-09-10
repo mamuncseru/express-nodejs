8408da1c0be482683c21ce2ac4a887409ff8a982
Md. Abdullah Al Mamun <mamun.cseian@gmail.com>
2021-09-10 08:38:46 +0600

    MVC learning

--- models/product.js
+++ models/product.js
@@ -1,5 +1,20 @@
 const fs = require("fs");
 const path = require("path");
+const p = path.join(
+    path.dirname(require.main.filename),
+    "data",
+    "products.json"
+  );
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      return cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
   constructor(title) {
@@ -7,36 +22,15 @@ module.exports = class Product {
   }
 
   save() {
-    const p = path.join(
-      path.dirname(require.main.filename),
-      "data",
-      "products.json"
-    );
-    fs.readFile(p, (err, fileContent) => {
-        let products = [];
-        if (!err) {
-            products = JSON.parse(fileContent);
-        }
-
-        products.push(this);
-        fs.writeFile(p, JSON.stringify(products), (err) => {
-            console.log(err);
-        })
+    getProductsFromFile((products) => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
     });
   }
 
   static fetchAll(cb) {
-    const p = path.join(
-        path.dirname(require.main.filename),
-        "data",
-        "products.json"
-      );
-      fs.readFile(p, (err, fileContent) => {
-        if (err) {
-
-            cb([]);
-        }
-        cb(JSON.parse(fileContent));
-      })
+    getProductsFromFile(cb);
   }
 };
